16467a239c4a98c08c6e0710dcef2b09da8d9bed
BazeBro1337 <taranee.cook@yandex.ru>
2024-10-28 15:56:00 +0300

    Add empty fabric mod template

---
+++ src/main/java/net/bzbr/tacticooly/TacticoolyArmorDataGenerator.java
@@ -0,0 +1,11 @@
+package net.bzbr.tacticooly;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TacticoolyArmorDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
